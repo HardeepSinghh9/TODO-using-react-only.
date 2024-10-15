0e1d3ec562d2c133dcbba13b5001ac4384522cca
HardeepSinghh9 <hardeepsinghpallaha@gmail.com>
2024-10-15 14:02:44 +0530

    tt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import { Slide } from "react-toastify";
+import { useEffect } from "react";
 function Header() {
+  function suuuc() {
+    return toast.info("Click on Item to mark/unmark completed", {
+      position: "top-right",
+      autoClose: 6500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+      transition: Slide,
+      rtl: false,
+    });
+  }
+  useEffect(() => {
+    suuuc();
+  }, []);
   return (
     <header>
+      <ToastContainer />
       <h1>To-Do App</h1>
     </header>
   );
